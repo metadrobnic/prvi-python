307b132f4f0e5683fcfdb1a411c2db7d0e20c951
Meta Drobnic <cajsna@gmail.com>
2016-10-23 16:23:33 +0200

    se ena verzija ugani stevilko

--- ugani_stevilko.py
+++ ugani_stevilko.py
@@ -17,3 +17,18 @@ while True:
         break
     else:
         print("St ni prava, ugibaj naprej")
+
+"""
+skrita_stevilka = 57
+
+while True:
+    stevilka = int(raw_input("Vpisi stevilko med 40 in 60 "))
+
+    if stevilka == skrita_stevilka:
+        print "Cestitamo!"
+        break
+    elif stevilka > skrita_stevilka:
+        print ("St ni prava, mora biti manjsa, ugibaj naprej")
+    elif stevilka < skrita_stevilka:
+        print ("St ni prava, mora biti vecja, ugibaj naprej")
+"""
